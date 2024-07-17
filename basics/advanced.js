ad631f3e3478e3256f013bab7437f8f0580991a9
shivendukrgupta23 <159147613+shivendukrgupta23@users.noreply.github.com>
2024-07-17 08:31:09 +0000

    advanced.js

---
+++ basics/advanced.js
@@ -0,0 +1,68 @@
+//console.log("Shivendu");
+//console.log(typeof bigNumber);
+
+
+//memory
+
+//Stack (Primitive) , heap(Non-Primitive)
+
+// let myYoutubename = "shivenduKumar" 
+ 
+// let anothername = myYoutubename
+
+// anothername= "chaiaurcode"
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+
+
+// let userOne = {
+//     email: "user@google.com",
+//     upi: "user@ybl"
+
+// }
+
+// let userTwo = userOne
+
+// userTwo.email= "shivaay@google.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
+
+//-------------------------------------------
+
+
+//Strings
+
+
+const name = "Shivaay"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value");
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Shivaay')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+
+
+//console.log(gameName.length);
+
+//console.log(gameName.toUpperCase());
+
+
+console.log(gameName.charAt(4));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
+const newStringOne = " Shivaay "
+console.log((newStringOne));
+console.log(newStringOne.trim());
