e3afa5500a8a19c4efb27eb8fe7b344b7202c8db
shivendukrgupta23 <159147613+shivendukrgupta23@users.noreply.github.com>
2024-07-17 09:01:39 +0000

    math.js

---
+++ basics/math.js
@@ -0,0 +1,51 @@
+//Number
+// const score = 400
+// console.log(score);
+// const balance  = new Number(100)
+// console.log(balance);
+
+//console.log(balance.toString.length);
+
+
+//const otherNumber= 123.8966
+
+//console.log(otherNumber.toPrecision(3));
+
+
+//const hundreds =1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//---------------------------------------
+
+//Maths
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1)); //decimal ke bd ke bhi digit hua to direct round to next number i.e, 4.1->5
+// console.log(Math.floor(4.9)); //decimal ke bd ke bhi digit hu to direct round integer value i.e->4.9->4
+
+
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,9));
+
+
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+console.log(Math.random()*10 + 1);
+
+
+const min = 10
+const max= 20
+
+console.log(Math.floor(Math.random() * (max-min + 1) + min));
+
+
+
+
+
+
